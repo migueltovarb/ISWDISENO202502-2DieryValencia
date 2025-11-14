b767fff059d77af437fdd71e26367045a783c756
DieryValencia <diery.valencia@campusucc.edu.co>
2025-11-14 12:03:14 -0500

    api Vehiculo

---
+++ API_Vehiculos/vehículo/vehículo/src/test/java/com/vehiculo/vehiculo/VehiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehiculo.vehiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
