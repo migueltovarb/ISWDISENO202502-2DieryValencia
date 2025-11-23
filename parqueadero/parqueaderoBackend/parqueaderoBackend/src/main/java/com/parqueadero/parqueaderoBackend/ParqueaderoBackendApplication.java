05fdd9f37ffe816e597f21ac2c8578f15160ca0d
DieryValencia <diery.valencia@campusucc.edu.co>
2025-11-23 00:25:23 -0500

    init spring boot

---
+++ parqueadero/parqueaderoBackend/parqueaderoBackend/src/main/java/com/parqueadero/parqueaderoBackend/ParqueaderoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.parqueadero.parqueaderoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueaderoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderoBackendApplication.class, args);
+	}
+
+}
---
+++ parqueadero/parqueaderoBackend/parqueaderoBackend/src/test/java/com/parqueadero/parqueaderoBackend/ParqueaderoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parqueadero.parqueaderoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueaderoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
